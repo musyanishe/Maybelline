b7ca75e1260c8d34c1e64fd590883835b01585b9
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-06-20 00:08:13 +0300

    add network manager (didn't finish)

---
+++ Services/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Maybelline
+//
+//  Created by Евгения Шевцова on 19.06.2022.
+//
+
+import Combine
+import Foundation
+
+class NetworkManager: ObservableObject {
+    
+}
