f18d3956b37a2d75a2f823c8e199e891b3a4c851
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-06-19 23:31:02 +0300

    Initial Commit

---
+++ Maybelline/MaybellineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaybellineApp.swift
+//  Maybelline
+//
+//  Created by Евгения Шевцова on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MaybellineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
