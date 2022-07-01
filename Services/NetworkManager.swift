d13b067d634cfa894ffabaca7819aa5f4f51c3f5
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-07-01 16:47:56 +0300

    edition of Networking layer

--- Services/NetworkManager.swift
+++ Services/NetworkManager.swift
@@ -8,29 +8,55 @@
 import Combine
 import Foundation
 
-class NetworkManager: ObservableObject {
-    @Published var maybelline = [Maybelline]()
+enum NetworkError {
+    case noData
+    case invalidURL
+    case decodingError
+}
+
+class NetworkManager: {
     
-    init() {
-        fetchData()
-    }
+    static let shared = NetworkManager()
     
-    func fetchData() {
-        guard let url = URL(string: "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline") else { return }
+    private init() {}
+    
+        //there is code for networking without async/await
+        /*
+         func fetchData() {
+         URLSession.shared.dataTask(with: url) { data, _, error in
+         guard let data = data else {
+         print(error?.localizedDescription ?? "No description error")
+         return
+         }
+         do{
+         let maybs = try JSONDecoder().decode([Maybelline].self, from: data)
+         DispatchQueue.main.async {
+         self.maybelline = maybs
+         }
+         } catch {
+         print(error.localizedDescription)
+         }
+         }.resume()
+         }
+         */
         
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No description error")
-                return
-            }
-            do{
-                let maybs = try JSONDecoder().decode([Maybelline].self, from: data)
-                DispatchQueue.main.async {
-                    self.maybelline = maybs
-                }
-            } catch {
-                print(error.localizedDescription)
-            }
-        }.resume()
+    //there is code for networking with async/await
+    func fetchMaybs async throws -> [Maybelline] {
+        
+        guard let url = URL(string: "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline") else { throw NetworkError.invalidURL
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        guard let maybs = JSONDecoder().decode([Maybelline].self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        return maybs
     }
+    
+    func fetchImageData(from url: String) throws -> Data {
+        guard let imageURL = URL(string: url) else { throw NetworkError.invalidURL }
+        guard let imageData = try? Data(contentsOf: imageURL) else { throw NetworkError.noData }
+        return imageData
+    }
+        
 }
