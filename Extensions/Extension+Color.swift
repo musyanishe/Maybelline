770e262faccbee623dd594d91a4f0c5ae1a7d530
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-07-03 16:19:04 +0300

    added extension for Color which help to use colors we needed easily

---
+++ Extensions/Extension+Color.swift
@@ -0,0 +1,23 @@
+//
+//  Extension+Color.swift
+//  Maybelline
+//
+//  Created by Евгения Шевцова on 03.07.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let secondaryText = Color("SecondaryText")
+    
+}
