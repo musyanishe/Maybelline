365ab5c2afbd54a9f3cbaefa4f83da2b3054b68b
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-07-03 17:22:29 +0300

    some editions

--- Views/AllProductsView.swift
+++ Views/AllProductsView.swift
@@ -9,12 +9,34 @@ import SwiftUI
 
 struct AllProductsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Maybelline on Well.ca")
+                    .foregroundColor(Color.theme.accent)
+                    .fontWeight(.medium)
+                    .font(.title)
+                Spacer()
+                
+                TabView {
+                    Rectangle()
+                    .frame(width: 300, height: 600)
+                    .cornerRadius(20)
+                    .foregroundColor(Color.theme.background)
+                    .shadow(radius: 20)
+                }
+                .tabViewStyle(.page(indexDisplayMode: .always))
+            }
+        }
     }
 }
 
 struct AllProductsView_Previews: PreviewProvider {
     static var previews: some View {
         AllProductsView()
+            .preferredColorScheme(.dark)
+            
     }
 }
